02e1c483964c50083a71cfb8fd44f3320b3b6678
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-11-06 10:02:08 +0000

    implemented user with id, email and password as well as a repository to find user.

---
+++ src/main/java/wishc1/wishlist/repository/AppUserRepository.java
@@ -0,0 +1,13 @@
+package wishc1.wishlist.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import wishc1.wishlist.model.AppUser;
+
+import java.util.Optional;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByEmail(String email); // For authentication by email
+}
+
